1ca930a833cfebebe171275d72d09ca1569aae75
Drifter <aminovtrabelsi@gmail.com>
2025-01-19 20:37:22 +0100

    small tweaks

--- scripts/next_level.gd
+++ scripts/next_level.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 func Teleport(body: Node2D) -> void:
-	if body is player:
+	if body.is_in_group("Player"):
 		(get_tree().root.get_node("LevelManager") as LevelManager).NextLevel()
