9f1f5ba85f7a5cceefc90a419d94d0b4e0985cd4
Erikson Silacier <silacier2@hotmail.com>
2025-01-19 16:30:42 -0300

    New adjustments

--- scenes/Rooms/area_2d.gd
+++ scenes/Rooms/area_2d.gd
@@ -3,9 +3,15 @@ extends Area2D
 var instance = preload("res://scenes/jeremiahbutgravity.tscn")
 @onready var base_room: Node2D = $"../.."
 
+var flag = false
+
 func _on_body_entered(body: Node2D) -> void:
+	if flag:
+		return
+	flag = true
 	print("woo")
 	body.visible=false
+	body.set_physics_process(false)
 	var jer = instance.instantiate()
-	jer.global_position = (body.global_position)
+	jer.global_position = (body.global_position + Vector2(0, -96))
 	base_room.call_deferred("add_child",jer)
